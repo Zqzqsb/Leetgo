fb63dac3b244e95c4374bdfbe73aac5258778165
ZQ <zqzqsb@gmail.com>
2024-09-03 14:07:46 +0800

    feat: test golang on twosum

---
+++ go/0001.two-sum/solution.go
@@ -0,0 +1,38 @@
+// Created by ZQ at 2024/09/03 13:55
+// leetgo: 1.4.8
+// https://leetcode.cn/problems/two-sum/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func twoSum(nums []int, target int) (ans []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				ans = append(ans, i, j)
+				return
+			}
+		}
+	}
+	return
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	nums := Deserialize[[]int](ReadLine(stdin))
+	target := Deserialize[int](ReadLine(stdin))
+	ans := twoSum(nums, target)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
